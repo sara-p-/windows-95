b409a180db59ff9338d6a249340bc93c3876556d
sara-p- <sarapitt88@gmail.com>
2024-03-18 22:24:49 -0400

    added a 'vaporwave from wiki' section with a purple and neon green grid background. Got rid of the 2nd title mask image, and replaced gsap with simpleparallax.js

--- src/js/modules/title-animation.js
+++ src/js/modules/title-animation.js
@@ -1,34 +1,29 @@
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
+// import { gsap } from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+// gsap.registerPlugin(ScrollTrigger)
+import simpleParallax from 'simple-parallax-js'
 
 export default function titleAnimation() {
   // variables
   const bgImage = document.querySelector('img.vaporwave-text__image')
   const titleMask = document.querySelector('.mask.mask--1')
+  const titleSection = document.querySelector('#title-section')
   const theBody = document.getElementById('body')
   const section1 = document.getElementById('section--1')
   const section2 = document.getElementById('section--2')
 
   // Background Movement
-  gsap.from(bgImage, {
-    scrollTrigger: {
-      trigger: section2,
-      scrub: true,
-      // toggleActions: 'restart pause resume pause',
-    },
-    yPercent: -25,
-    ease: 'none',
-  })
-
-  // // Text Movement
-  // gsap.to(titleMask, {
+  // gsap.from(bgImage, {
   //   scrollTrigger: {
-  //     trigger: section2,
-  //     end: 'middle 50%',
-  //     scrub: true,
+  //     trigger: titleSection,
+  //     scrub: 1,
+  //     // toggleActions: 'restart pause resume pause',
   //   },
-  //   scale: 0,
+  //   yPercent: -25,
   //   ease: 'none',
   // })
+
+  new simpleParallax(bgImage, {
+    orientation: down,
+  })
 }
