affeb0f0d5c29ef22b592619adb1ff56c434d00f
sara-p- <sarapitt88@gmail.com>
2024-03-20 14:17:31 -0400

    working on the sunset illustration animations

--- src/js/modules/title-animation.js
+++ src/js/modules/title-animation.js
@@ -1,29 +1,43 @@
-// import { gsap } from 'gsap'
-// import { ScrollTrigger } from 'gsap/ScrollTrigger'
-// gsap.registerPlugin(ScrollTrigger)
-import simpleParallax from 'simple-parallax-js'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+// import simpleParallax from 'simple-parallax-js'
 
 export default function titleAnimation() {
   // variables
   const bgImage = document.querySelector('img.vaporwave-text__image')
+  const roadBackground = document.querySelector('img.road--stripe-background')
   const titleMask = document.querySelector('.mask.mask--1')
   const titleSection = document.querySelector('#title-section')
   const theBody = document.getElementById('body')
   const section1 = document.getElementById('section--1')
   const section2 = document.getElementById('section--2')
+  const section4 = document.getElementById('section--4')
 
   // Background Movement
-  // gsap.from(bgImage, {
-  //   scrollTrigger: {
-  //     trigger: titleSection,
-  //     scrub: 1,
-  //     // toggleActions: 'restart pause resume pause',
-  //   },
-  //   yPercent: -25,
-  //   ease: 'none',
-  // })
-
-  new simpleParallax(bgImage, {
-    orientation: down,
+  gsap.from(bgImage, {
+    scrollTrigger: {
+      trigger: titleSection,
+      scrub: 1,
+    },
+    yPercent: -25,
+    ease: 'none',
   })
+  // Background Movement
+  gsap.from(roadBackground, {
+    scrollTrigger: {
+      trigger: section4,
+      scrub: 1,
+    },
+    yPercent: -25,
+    ease: 'none',
+  })
+
+  // new simpleParallax(bgImage, {
+  //   orientation: 'down',
+  //   overflow: true,
+  // })
+  // new simpleParallax(roadBackground, {
+  //   orientation: 'down',
+  // })
 }
