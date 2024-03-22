0406428c219995cc5947078e205c0fba2c8f433e
sara-p- <sarapitt88@gmail.com>
2024-03-21 20:24:55 -0400

    removing images I don't need and adding the ones I do. Also working on the sunset animation

--- src/js/modules/animations.js
+++ src/js/modules/animations.js
@@ -6,12 +6,11 @@ gsap.registerPlugin(ScrollTrigger)
 export default function animations() {
   // variables
   const bgImage = document.querySelector('img.vaporwave-text__image')
-  const roadBackground = document.querySelector('img.road--stripe-background')
-  const titleMask = document.querySelector('.mask.mask--1')
+  const roadBackground = document.querySelector('.road-stripes')
+  const sun = document.querySelector('.sun')
+  const mountainLeft = document.querySelector('.mountain.back-left')
+  const mountainRight = document.querySelector('.mountain.back-right')
   const titleSection = document.querySelector('#title-section')
-  const theBody = document.getElementById('body')
-  const section1 = document.getElementById('section--1')
-  const section2 = document.getElementById('section--2')
   const section4 = document.getElementById('section--4')
 
   // Background Movement
@@ -23,13 +22,45 @@ export default function animations() {
     yPercent: -25,
     ease: 'none',
   })
-  // Background Movement
+  // Sunset ground movement
   gsap.from(roadBackground, {
     scrollTrigger: {
       trigger: section4,
       scrub: 1,
     },
-    yPercent: -25,
+    yPercent: -60,
+    ease: 'none',
+  })
+  // Sun movement
+  gsap.to(sun, {
+    scrollTrigger: {
+      trigger: section4,
+      scrub: 1,
+    },
+    yPercent: 50,
+    scale: 2,
+    ease: 'none',
+  })
+  // mountain-left movement
+  gsap.to(mountainLeft, {
+    scrollTrigger: {
+      trigger: section4,
+      scrub: 1,
+    },
+    scale: 2,
+    xPercent: -20,
+    yPercent: -50,
+    ease: 'none',
+  })
+  // mountain-right movement
+  gsap.to(mountainRight, {
+    scrollTrigger: {
+      trigger: section4,
+      scrub: 1,
+    },
+    scale: 2,
+    xPercent: 25,
+    yPercent: -50,
     ease: 'none',
   })
 
