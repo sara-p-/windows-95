7e672fc7acf91803c4b9af774798f905d0c2d352
sara-p- <sarapitt88@gmail.com>
2024-04-09 10:33:40 -0400

    working on the wiki animations

--- src/js/modules/wiki-animations.js
+++ src/js/modules/wiki-animations.js
@@ -1,10 +1,18 @@
 export default function wikiAnimations() {
   const wikis = document.querySelectorAll('.wiki__box')
-  const wikiObj = Array.from(wikis).map((wiki) => {
-    const compuStyles = window.getComputedStyle(wiki)
-    const translateAmount = compuStyles.getPropertyValue('transform')
-    const button = wiki.querySelector('.wiki__button')
 
-    console.log(translateAmount)
+  const wikiArray = Array.from(wikis).map((wiki) => {
+    const compuStyles = window.getComputedStyle(wiki)
+    const zIndex = compuStyles.zIndex
+    const lag = wiki.getAttribute('data-lag')
+    return { wiki, zIndex, lag }
+  })
+
+  // console.log(wikiArray)
+
+  wikiArray.forEach(({ wiki, zIndex, lag }, index) => {
+    const button = wiki.querySelector('button')
+
+    button.addEventListener('click', (e) => {})
   })
 }
