905095792453ff6146a4fd6e34c3dc411d18823b
sara-p- <sarapitt88@gmail.com>
2024-07-01 21:22:43 -0400

    working on the wiki section

---
+++ src/js/data/wiki-data.js
@@ -0,0 +1,26 @@
+export const wikiData = [
+  {
+    id: 0,
+    heading: 'What is Vaporwave?',
+    subheading: 'According to wikipedia',
+    copy: 'Vaporwave is a microgenre of electronic music and a subgenre of hauntology, a visual art style, and an Internet meme that emerged in the early 2010s, and became well-known in 2015.',
+  },
+  {
+    id: 1,
+    heading: 'How is vaporwave defined?',
+    subheading: '',
+    copy: 'It is defined partly by its slowed-down, chopped and screwed samples of smooth jazz, 1970s elevator music, R&B, and lounge music from the 1980s and 1990s.',
+  },
+  {
+    id: 2,
+    heading: `What's the surrounding subculture like?`,
+    subheading: ``,
+    copy: 'The surrounding subculture is sometimes associated with an ambiguous or satirical take on consumer capitalism[33] and pop culture, and tends to be characterized by a nostalgic or surrealist engagement with the popular entertainment, technology and advertising of previous decades.',
+  },
+  {
+    id: 3,
+    heading: 'What is Vaporwave like visually?',
+    subheading: '',
+    copy: 'Visually, it incorporates early Internet imagery, late 1990s web design, glitch art, anime, stylized Greek sculptures, 3D-rendered objects, and cyberpunk tropes in its cover artwork and music videos.',
+  },
+]
